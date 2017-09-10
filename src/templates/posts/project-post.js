19217557359a82fe307f4aeef2e3bf91689464a5
Bret Finley <bretafinley@gmail.com>
2017-09-10 15:31:27 -0600

    Cleaned up project view.

--- src/templates/posts/project-post.js
+++ src/templates/posts/project-post.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import Link from 'gatsby-link';
+import moment from 'moment';
+import {FaFolder, FaGithub, FaCalendar} from 'react-icons/lib/fa';
+
+import Tag from '../../components/tag';
 
 // strip data off of the object the callee passes in
 export default function Template({data}) {
-    // equivalent to post = data.markdownRemark;
     const { markdownRemark: post } = data;
+    const renderedTags = handleTags(post.frontmatter.tags);
+    const formattedDate = moment(post.frontmatter.post_date, "YYYYMMDD").format('ll');
     return (
         <div className="post">
             <h1 className="post-title">{post.frontmatter.title}</h1>
             <h3 className="post-subtitle">{post.frontmatter.subtitle}</h3>
+            <div className="post-meta">
+                <div><FaCalendar /> {formattedDate}</div>
+                <FaFolder /> <ol className="breadcrumb">
+                    <li><Link to="/projects">Projects</Link></li>
+                    <li><Link to={`/category/${post.frontmatter.category}`}>{post.frontmatter.category}</Link></li>
+                    <li className="active"><Link to={`/folder/${post.frontmatter.category}/${post.frontmatter.folder}`}>{post.frontmatter.folder}</Link></li>
+                </ol>
+                <div><FaGithub /> <Link to={post.frontmatter.subject_url}>{post.frontmatter.subject_url}</Link></div>
+            </div>
+            <hr />
             <div className="post-body" dangerouslySetInnerHTML={{__html: post.html}}></div>
+            <div className="post-tags">{renderedTags}</div>
         </div>
     );
+}
+
+function handleTags(tags) {
+    return tags.map((tag, index) => {
+        return (
+            <Tag key={index} text={tag} />
+        );
+    });
 }
 
 export const postQuery = graphql`
