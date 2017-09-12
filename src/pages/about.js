5844af42bf70cd7dac96b8c5cc51b2f25b61e2fa
Bret Finley <bretafinley@gmail.com>
2017-09-11 19:43:14 -0600

    Added starter to about.

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,9 @@ function About() {
   return (
     <div>
       <h2 className="page-title">About Me</h2>
+      <div className="about-body">
+        Hi! My name is Bret Finley. I'm a software devloper living in beautiful Boise, Idaho.
+      </div>
     </div>
   );
 }
